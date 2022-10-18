5da11443eb4f30fc746a421bf7f64935be95f2f0
zuils <vijaylemon05@gmail.com>
2022-10-18 01:21:40 -0400

    fixed light mode and dark mode

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 function changeTheme() {
-    var selectedId = $('input[name="theme"]:checked').attr('id');
-    $("input[type=radio][" + selectedId + "]").prop("disabled", true);
-    if (localStorage) localStorage.setItem("darkmode", $("#darkmode").is(":checked"));
-}
+	const toggle = $("#lightmode").get(0);
+	const body = document.querySelector("body");
 
-$(function() {
-    if (localStorage) {
-        $("#dark").prop("checked", localStorage.getItem("darkmode")!=="false");
-    };
-});
+	toggle.addEventListener("click", function() {
+		this.classList.toggle("bi-brightness-high-fill");
+		if (this.classList.toggle("bi-moon-fill")) {
+			$("#theme-light").prop("disabled", false);
+			$("#theme-dark").prop("disabled", true);
+		} else {
+			$("#theme-light").prop("disabled", true);
+			$("#theme-dark").prop("disabled", false);
+		}
+	})
+}
